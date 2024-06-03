cf7c1b6f93024235804e85f5bf763c78d6b7e6e9
alx <aleksandar.zedi@gmail.com>
2024-06-03 13:05:26 +0200

    Refactored withTestId hoc for typesafety

--- src/components/withTestId.tsx
+++ src/components/withTestId.tsx
@@ -1,9 +1,9 @@
 // libs
 import React from 'react';
 
-type Props = {
-  children: React.ReactNode;
-};
+interface TestIdProps {
+  children?: React.ReactNode;
+}
 
 /**
  * withTestId HOC that adds a data-testid attribute to the wrapped component
@@ -13,14 +13,19 @@ type Props = {
  * @param {string} testId The value for the data-testid attribute.
  * @returns {React.FC} A new component with the data-testid attribute added.
  */
-function withTestId(WrappedComponent, testId) {
-  return function TestIdWrapper({ children, ...props }: Props) {
+function withTestId<P>(
+  WrappedComponent: React.ComponentType<P>,
+  testId: string
+) {
+  return function TestIdWrapper(props: P & TestIdProps) {
+    const { children, ...restProps } = props;
+
     const enhancedChildren = React.Children.map(children, (child) => {
       return child;
     });
 
     return (
-      <WrappedComponent {...props} data-testid={testId}>
+      <WrappedComponent {...(restProps as P)} data-testid={testId}>
         {enhancedChildren}
       </WrappedComponent>
     );
